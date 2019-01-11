bd687fab929c541510f2001a5bf78146063e1150
[redacted] <r3dact3d@users.noreply.github.com>
2019-01-11 13:41:12 -0600

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import requests
+import json
+from sys import argv
+text = ''
+argv.pop(0)
+for i in argv:
+    text = text +" " + i
+params = { 'text' : text }
+langList = 'ermahgerd brooklyn uk2us us2uk morse cockney fudd dolan jive vulcan shakespeare oldenglish chef huttese mandalorian gungan cheunh sith quenya sindarin valyrian dothraki piglatin ferblatin minion valspeak pirate yoda'
+print langList
+lang = raw_input('Pick language: ')
+if lang not in langList:
+     print 'You didn\'t pick a language from the list!'
+     exit()
+url = 'http://api.funtranslations.com/translate/' + lang + '.json'
+page = requests.get(url, params=params)
+#print page.url
+j = page.json()
+print "#######################################################"
+print j['contents']['translated']
