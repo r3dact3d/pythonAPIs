7313a9fd05f726bcb75dcb68f6b175fc461382c8
r3dact3d <redhatbrady@gmail.com>
2025-01-22 09:16:06 -0600

    push changes

--- spaceWhois.py
+++ spaceWhois.py
@@ -2,14 +2,21 @@
 import requests
 
 url = 'http://api.open-notify.org/astros.json'
-response = requests.get(url).json()
-number = response["number"]
-people = response["people"]
-return = f"There are {number} people in space."
-print("==================================")
-print(f"Humans in space currently: {number}")
-for i in range(0, number):
-    boat = people[i]["craft"]
-    name = people[i]["name"]
-    print(f"Craft: {boat}             Name: {name}")
-    print("---------------------------------------")
+response = requests.get(url).text
+print(f"""
+Provide information regarding :param people:he details below concerning astronauts, humans, and people in space. 
+Provide summary and interesting information regarding the craft the person is on.
+Report the number as the number of people in space.
+{response}
+""")
+#response = requests.get(url).json()
+#number = response["number"]
+#people = response["people"]
+#return = f"There are {number} people in space."
+#print("==================================")
+#print(f"Humans in space currently: {number}")
+#for i in range(0, number):
+#    boat = people[i]["craft"]
+#    name = people[i]["name"]
+#    print(f"Craft: {boat}             Name: {name}")
+#    print("---------------------------------------")
--- tools_spaceWhois.py
+++ tools_spaceWhois.py
@@ -1,32 +1,36 @@
 """
 title: Space Whois
 author: Brady Thompson (mailto:redactedtech@gmail.com)
-author_url: https://github.com/r3dact3d
+author_url: https://r3dact3d.github.io
+git_url: http://github.com/r3dact3d
 version: 0.2.0
-
-This module defines a Tools class that looks up humans in space
+description: This module defines a Tools class that looks up humans in space
 """
 
 import requests
 from pydantic import BaseModel, Field
+from typing import List
 
 
 class Tools:
-    class Valves(BaseModel):
-        pass
-
-    class UserValves(BaseModel):
-        pass
+    _valves: 'Tools'.Valves = None
+    _user_valves: 'Tools'.UserValves = None
 
     def __init__(self):
-        self.valves = self.Valves()
-        self.user_valves = self.UserValves()
+        self._valves = self.Valves()
+        self._user_valves = self.UserValves()
 
     def get_humans_space(self) -> str:
-        url = "http://api.open-notify.org/astros.json"
+        """
+        Get number of humans in space from API response.
+
+        :param url: URL of the API to retrieve data from
+        :return: The number of humans in space, a dictionary with the names of humans and crafts, and a list of astronauts
+        """
+        # ...
         response = requests.get(url).json()
-        number = response["number"]
-        people = response["people"]
-        return (
-            f"Humans in space currently: {number} and here are details on the {people}"
-        )
+        number_of_people = response.get("number")
+        astronauts = [person["name"] for person in response.get("people")]
+        crafts = [craft["name"] for craft in response.get("crafts")]
+        return f"""
+        Give a description of the{number_of_people} humans in space, {astronauts} astronauts, on {crafts} spacecraft."""
